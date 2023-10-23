1b597ef63876e26fbcb72fd09d440f16d9f90b19
Victor Lima <victord2704@gmail.com>
2023-10-23 20:03:35 -0300

    Upload

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener ('load', function() {
+    const texto = document.querySelector('.chamada-texto')
+    const digita = "Ingressando no mundo da Tecnologia e realizando um sonho!"
+    let indice = 0
+
+    const intervalo = this.setInterval(function() {
+        texto.textContent += digita[indice]
+        indice++
+        if (indice >= digita.length) {
+            clearInterval(intervalo)
+        }
+    }, 30)
+
+})
